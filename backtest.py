bb86eef70d95f43754599537a4aa81496bab82f3
Matthew Ingersoll <matth@mtingers.com>
2022-02-05 19:45:00 +0000

    Update backtester and logs

--- backtest.py
+++ backtest.py
@@ -1,7 +1,5 @@
 """Backtest using the 1m-olhcv csv files
 
-Sorry for the mess.
-
 emaA=1,emaB=2 tested the best from the bruteforce.
 """
 import sys
@@ -14,82 +12,41 @@ import glob
 import json
 from random import choice
 import warnings
+import argparse
+from dataclasses import dataclass
+from decimal import Decimal
+from tqdm import tqdm
+from tabulate import tabulate
 
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
-FEE = 0.6/100
+FEE = Decimal(0.6/100)
+
+class Stats:
+    wallet: Decimal = Decimal('1000.00')
+    losses: int = 0
+    wins: int = 0
+    per_day: dict = {
+        'fee':{}, 'net_profit':{}, 'percent':{}
+    }
+    sell_log: list = []
 
 def pdiff(old, new):
-    try:
-        return ((float(new) - float(old)) / float(old)) * float('100.0')
-    except:
-        print('OLD:', old)
-        print('NEW:', new)
-        old_first = old
-        new_first = new
-        #if type(old) != float:
-        #    old_first = old['close'].iloc(0)
-        new_first = new[0]['close']
-        return pdiff(old_first, new_first)
+    return ((Decimal(new) - Decimal(old)) / Decimal(old)) * Decimal('100.0')
 
-def backtest2(do_print=True, emaA=12, emaB=26):
-    # 2) Process history csv file
-    df = pd.read_csv(sys.argv[1])
-    df.timestamp = pd.to_datetime(df.timestamp, unit='s')
-    df = df.set_index("timestamp")
-    df = df.drop(columns=['open','high','low','volume'])
-    df = df.resample('1h').ohlc()
-    df['open'] = df['close']['open']
-    df['high'] = df['close']['high']
-    df['low'] = df['close']['low']
-    df['close2'] = df['close']['close']
-    #df['close2'] = df['close']['low']
-    df = df.drop(columns=['close'])
-    df['close'] = df['close2']
-    df = df.drop(columns=['close2'])
-    idf = df.resample('1D').ohlc()
-    df['emaA'] = ta.ema(idf['close']['']['close'], length=emaA)
-    df['emaB'] = ta.ema(idf['close']['']['close'], length=emaB)
-    df.fillna(method='ffill', inplace=True)
-    df.dropna(axis='rows', how='any', inplace=True)
-    df = df.drop(columns=['open', 'high', 'low'])
-    print(df)
-
-    # 3) Get last row and assign close price, emaA, emaB
-    last_row = df.iloc[-1]
-    #print(last_row)
-    close = last_row['close'].item()
-    emaA = last_row['emaA'].item()
-    emaB = last_row['emaB'].item()
-    print('last_row:')
-    print(last_row)
-    if emaA > emaB:
-        print('BUYABLE')
-    elif emaB > emaA:
-        print('SELLABLE')
-    else:
-        print('NOOPABLE')
-
-def backtest1(do_print=True, emaA=12, emaB=26):
-    df = pd.read_csv(sys.argv[1])
-    df_test = pd.read_csv(sys.argv[1])
-    # filter by date range here:
-    # 2018-01-01 00:00:00
-    #df = df[~(df['timestamp'] < 1514764800)]
-    # 2020-01-01 01:00:00
-    #df = df[~(df['timestamp'] < 1577883600)]
+def get_dataframes(csv_file, emaA=1, emaB=2):
+    df = pd.read_csv(csv_file)
+    df_test = pd.read_csv(csv_file)
     df_test.timestamp = pd.to_datetime(df_test.timestamp, unit='s')
     df.timestamp = pd.to_datetime(df.timestamp, unit='s')
     df_test = df_test.set_index("timestamp")
     df = df.set_index("timestamp")
     df = df.drop(columns=['open','high','low','volume'])
     df = df.resample('1h').ohlc()
-    # flatten
     df['open'] = df['close']['open']
     df['high'] = df['close']['high']
     df['low'] = df['close']['low']
     df['close2'] = df['close']['close']
-    #df['close2'] = df['close']['low']
     df = df.drop(columns=['close'])
     df['close'] = df['close2']
     df = df.drop(columns=['close2'])
@@ -98,104 +55,117 @@ def backtest1(do_print=True, emaA=12, emaB=26):
     df['emaB'] = ta.ema(idf['close']['']['close'], length=emaB)
     df.fillna(method='ffill', inplace=True)
     df.dropna(axis='rows', how='any', inplace=True)
-    if do_print:
-        print(df)
-    losses = 0
-    wins = 0
-    # end flatten
-    profits = {}
-    profits_p = {}
-    wallet = 1000.00
+    return df
+
+
+def huf(f: Decimal):
+    return '{:,.2f}'.format(f)
+
+def _backtest(df, emaA: int = 1, emaB: int = 2, progress_bar: tqdm = None, debug: bool = False):
+    stats = Stats()
     bought = None
     fee = None
-    ema_x_buy = 0
-    ema_x_sell = 0
-    for (dt, r) in df.iterrows():
-        close = r['close'].item()
-        emaA = r['emaA'].item()
-        emaB = r['emaB'].item()
-        try:
-            test = df_test.loc[str(dt)]
-        except:
-            cur_dt = str(dt)
-            end_dt = cur_dt.replace('00:00', '09:00')
-            test = df_test.loc[cur_dt :end_dt]
-        cur_price = test['close']
+    for (timestamp, row) in df.iterrows():
+        close = row['close'].item()
+        emaA = row['emaA'].item()
+        emaB = row['emaB'].item()
+        cur_price = Decimal(close)
         if not bought and emaA > emaB:
-            #bought = close
             bought = cur_price
-            size = wallet / bought
-            fee = wallet * FEE
-            if do_print:
-                print('{} BUY:, {:,.2f} wallet={:,.2f} size={:,.4f} emaA={:.2f} emaB={:.2f}'.format(
-                    dt, bought, wallet, size, emaA, emaB))
+            size = stats.wallet / bought
+            fee = stats.wallet * FEE
+            if debug:
+                print('{} BOUGHT: price={:,.2f} A={:,.2f} B={:,.2f} '
+                    'size={:,.2f} fee={:,.2f} wallet={:,.2f}'.format(
+                        timestamp, cur_price, emaA,
+                        emaB, size, fee, stats.wallet))
         elif bought and emaB > emaA:
-            #sell = close
             sell = cur_price
-            try:
-                px = pdiff(bought, sell)
-            except:
-                print('---------------------------------------- fixup -------------')
-                sell = close
-                px = pdiff(bought, close)
-                
-            #profit = ((sell * size) - wallet)
-            #wallet = (wallet + profit) - fee
-            profit = wallet * ((px/100.)-FEE)
-            wallet = wallet + profit
-            #fee = wallet * FEE
-            if profit > 0:
-                wins += 1
+            percent = pdiff(bought, sell)
+            net_profit = stats.wallet * ((percent/100)-FEE)
+            stats.wallet = stats.wallet + net_profit
+            if debug:
+                print('{} SOLD : price={:,.2f} A={:,.2f} B={:,.2f} '
+                    'profit={:,.2f} percent={:,.2f} wallet={:,.2f}'.format(
+                        timestamp, cur_price, emaA,
+                        emaB, net_profit, percent, stats.wallet))
+            stats.sell_log.append(
+                (timestamp, huf(bought),
+                    huf(cur_price), huf(net_profit), huf(percent), huf(stats.wallet)))
+            if net_profit > 0:
+                stats.wins += 1
             else:
-                losses += 1
-            if do_print:
-                print(
-                        "TEST", dt, 'SELL:', sell, wallet, bought, profit, fee, px, emaA, emaB)
-                print('{} SELL:, {:,.2f} wallet={:,.2f} bought={:,.2f} profit={:,.2f} fee={:,.2f} p={:,.2f} emaA={:.2f} emaB={:.2f}'.format(
-                    dt, sell, wallet, bought, profit, fee, px, emaA, emaB))
+                stats.losses += 1
             bought = None
-            pdate = str(dt).rsplit('-', 1)[0]
-            if not pdate in profits:
-                profits[pdate] = profit
-                profits_p[pdate] = [px]
-            else:
-                profits[pdate] += profit
-                profits_p[pdate].append(px)
-    if do_print:
-        for k, v in profits.items():
-            print('{} {:,.2f} {:,.2f}%'.format(k, v, sum(profits_p[k])))
-    pvals = []
-    pvals_month = []
-    for i in profits_p.values():
-        for j in i:
-            pvals.append(j)
-    for k, v in profits_p.items():
-        pvals_month.append(sum(v))
-    print('mean-p: {:,.2f}%'.format(np.mean(pvals)))
-    print('median-p: {:,.2f}%'.format(np.median(pvals)))
-    print('mean-month-p: {:,.2f}%'.format(np.mean(pvals_month)))
-    print('median-month-p: {:,.2f}%'.format(np.median(pvals_month)))
-    print('wins:', wins, 'losses:', losses)
-    return wallet
+            year_month_day = str(timestamp).rsplit('-', 1)[0]
+            if not year_month_day in stats.per_day['fee']:
+                stats.per_day['fee'][year_month_day] = []
+            if not year_month_day in stats.per_day['percent']:
+                stats.per_day['percent'][year_month_day] = []
+            if not year_month_day in stats.per_day['net_profit']:
+                stats.per_day['net_profit'][year_month_day] = []
+            stats.per_day['fee'][year_month_day].append(fee)
+            stats.per_day['percent'][year_month_day].append(percent)
+            stats.per_day['net_profit'][year_month_day].append(net_profit)
+        progress_bar.update(1)
+    return stats
+
+def backtest(emaA: int = 1, emaB: int = 2, csv_file: str = None):
+    df = get_dataframes(csv_file, emaA=emaA, emaB=emaB)
+    with tqdm(total=len(df)) as progress_bar:
+        stats = _backtest(df, emaA=emaA, emaB=emaB, progress_bar=progress_bar)
+    return stats
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--csv-file',
+        help='Path to OHLC CSV file', dest='csv_file', required=True)
+    args = parser.parse_args()
+    stats = backtest(emaA=1, emaB=2, csv_file=args.csv_file)
+    fee_total = sum([sum(i) for i in stats.per_day['fee'].values()])
+    percent_total = sum([sum(i) for i in stats.per_day['percent'].values()])
+    net_profit_total = sum([sum(i) for i in stats.per_day['net_profit'].values()])
+    percent_mean = np.mean([sum(i) for i in stats.per_day['percent'].values()])
+    net_profit_mean = np.mean([sum(i) for i in stats.per_day['net_profit'].values()])
+    percent_median = np.median([sum(i) for i in stats.per_day['percent'].values()])
+    net_profit_median = np.median([sum(i) for i in stats.per_day['net_profit'].values()])
+    day_results = []
+    print('Sell log:')
+    print(tabulate(stats.sell_log, tablefmt='fancy_grid', headers=[
+        'Date', 'Buy Price', 'Sell Price', 'Net Profit', 'Percent', 'Wallet']))
+    print('Month breakdown (percent):')
+    for timestamp in stats.per_day['fee'].keys():
+        day_results.append((
+            timestamp,
+            #'{:,.2f}'.format(sum(stats.per_day['fee'][timestamp])),
+            #'{:,.2f}'.format(np.mean(stats.per_day['fee'][timestamp])),
+            #'{:,.2f}'.format(np.median(stats.per_day['fee'][timestamp])),
+            '{:,.2f}'.format(sum(stats.per_day['percent'][timestamp])),
+            '{:,.2f}'.format(np.mean(stats.per_day['percent'][timestamp])),
+            '{:,.2f}'.format(np.median(stats.per_day['percent'][timestamp])),
+            len(stats.per_day['percent'][timestamp]),
+            #'{:,.2f}'.format(sum(stats.per_day['net_profit'][timestamp])),
+            #'{:,.2f}'.format(np.mean(stats.per_day['net_profit'][timestamp])),
+            #'{:,.2f}'.format(np.median(stats.per_day['net_profit'][timestamp])),
+        ))
+
+    print(tabulate(day_results, tablefmt='fancy_grid', headers=[
+        'Date',
+        #'Fees', 'FeesMean', 'FeesMedian',
+        'Percent', 'Percent Mean', 'Percent Median',
+        #'NetProfitMean', 'NetProfitMedian', 'NetProfit',
+        'Transactions',
+        ])
+    )
+    results = {
+        'Monthly Mean Percent':[huf(percent_mean)],
+        'Monthly Median Percent':[huf(percent_median)],
+        'Wins':[stats.wins],
+        'Losses':[stats.losses],
+        'Total Net Profit':[huf(net_profit_total)],
+    }
+    print('Results:')
+    print(tabulate(results, tablefmt='fancy_grid', headers='keys'))
 
 if __name__ == '__main__':
-    combinations = []
-    # emaA=1,emaB=2 tested the best from the bruteforce below
-    result = backtest1(emaA=1, emaB=2, do_print=True)
-    #result = backtest1(emaA=1, emaB=2, do_print=True)
-    print('{:,.2f}'.format(result))
-    sys.exit(0)
-    # Bruteforce to find best combinations of emaA/emaB
-    for i in range(1, 26):
-        for j in range(1, 26):
-            combinations.append((i, j))
-    results = {}
-    for (emaA, emaB) in combinations:
-        key = '{},{}'.format(emaA, emaB)
-        if not key in results:
-            result = test5(emaA=emaA, emaB=emaB, do_print=False)
-            results[key] = result
-            print(emaA, emaB, '->', '{:,.2f}'.format(result))
-    for k,v in results.items():
-        print(k, '{:,.2f}'.format(v))
-
+    main()
