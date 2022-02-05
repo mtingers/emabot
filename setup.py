f2eef5b63030d991f245df2b59207ce4553dbe45
Matthew Ingersoll <matth@mtingers.com>
2022-02-05 19:47:46 +0000

    Create backtest command

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages
 
 setup(
     name='EmaBot',
-    version='1.0.0',
+    version='1.1.0',
     author='Matth Ingersoll',
     author_email='matth@mtingers.com',
     packages=find_packages(),
-    license='GPLv3',
+    license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/mtingers/emabot',
@@ -24,11 +24,11 @@ setup(
     entry_points={
         'console_scripts': [
             'emabot=emabot.bot:main',
+            'backtest=emabot.backtest:main',
         ],
     },
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
