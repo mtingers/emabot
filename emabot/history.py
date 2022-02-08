e621c334873178592717410c618427652ee677b2
Matthew Ingersoll <matth@mtingers.com>
2022-02-08 02:59:39 +0000

    Add debug to generate_historical_csv

--- emabot/history.py
+++ emabot/history.py
@@ -15,7 +15,7 @@ def date2str(dtobj_or_str):
     """Convert date to str"""
     return str(dtobj_or_str).replace(' ', 'T').split('.')[0]+'Z'
 
-def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
+def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522, debug=False):
     """Generate CSV file from coinbase get_product_historic_rates.
     Append to existing file if it exists.
     """
@@ -23,9 +23,14 @@ def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
     public_client = cbpro.PublicClient()
     last_date = None
     first_date = None
+    prev_data = []
     if os.path.exists(outfile):
-        #print('reading prev file')
+        if debug:
+            print('reading prev file')
         prev_data = open(outfile).read().strip().split('\n')
+    if len(prev_data) > 10:
+        if debug:
+            print('prev_data length:', len(prev_data))
         first_date = float(prev_data[1].split(',')[0].replace('"', ''))
         last_date = float(prev_data[-1].split(',')[0].replace('"', ''))
         diff_first = int((time.time() - first_date) / 86400) + 86400
@@ -39,23 +44,30 @@ def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
             out_fd = open(outfile, 'w')
             out_fd.write('"timestamp","low","high","open","close","volume"\n')
         else:
-            #print('appending to file')
+            if debug:
+                print('appending to file')
             start_date = datetime.datetime.now() - timedelta(days=diff_last+1)
             next_date = start_date #  + timedelta(minutes=SIZE)
             out_fd = open(outfile, 'a')
         end_date = datetime.datetime.now()+timedelta(days=1)
     else:
+        if debug:
+            print('truncate file, new data')
         start_date = datetime.datetime.now() - timedelta(days=days_ago)
         end_date = datetime.datetime.now()+timedelta(days=1)
         next_date = start_date # + timedelta(minutes=SIZE)
         out_fd = open(outfile, 'w')
         out_fd.write('"timestamp","low","high","open","close","volume"\n')
 
+    if debug:
+        print('start_date:', start_date, 'next_date:', end_date)
     while next_date < end_date:
         start_str = date2str(next_date)
         end_str = date2str(next_date+timedelta(hours=4))
         stats = None
         error = False
+        if debug:
+            print('start_str:', start_str, 'end_str:', end_str)
         for attempt in range(14):
             try:
                 stats = public_client.get_product_historic_rates(
@@ -64,6 +76,8 @@ def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
                     start=date2str(next_date),
                     end=date2str(next_date+timedelta(hours=4)))
                 error = False
+                if debug:
+                    print('stats length:', len(stats))
                 break
             except json.decoder.JSONDecodeError as e:
                 print(e)
@@ -79,6 +93,8 @@ def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
         # stats are from newest to oldest, so reverse it
         stats.reverse()
         if len(stats) < 3:
+            if debug:
+                print('len(stats) < 3. Could mean there is no data in this range or it completed.')
             break
         for i in stats:
             (tstamp, low, high, x_open, x_close, x_volume) = i
@@ -88,7 +104,7 @@ def generate_historical_csv(outfile, pair='BTC-USD', days_ago=522):
             out_fd.write('"{}","{}","{}","{}","{}","{}"\n'.format(
                 tstamp, low, high, x_open, x_close, x_volume))
             last_date = tstamp
-        time.sleep(2)
+        time.sleep(2.0)
     out_fd.close()
 
 def main():
