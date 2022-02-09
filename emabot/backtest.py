4a5455421781917d82c0a57a83c6a667131ea8fb
Matthew Ingersoll <matth@mtingers.com>
2022-02-09 08:49:11 +0000

    Modularize backtest strats

--- emabot/backtest.py
+++ emabot/backtest.py
@@ -2,18 +2,132 @@
 
 emaA=1,emaB=2 tested the best from the bruteforce.
 """
-import warnings
+from abc import ABCMeta, abstractmethod
+from abc import ABC
 import argparse
+import warnings
 from decimal import Decimal
-from tqdm import tqdm
 from tabulate import tabulate
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
-from .util import *
+from .util import huf, pdiff, import_class
 
 warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
 
+FEE = Decimal(0.6/100)
+
+class Stats:
+    wallet: Decimal = Decimal('1000.00')
+    losses: int = 0
+    wins: int = 0
+    per_day: dict = {
+        'fee':{}, 'net_profit':{}, 'percent':{}
+    }
+    sell_log: list = []
+
+class BacktestBase(ABC):
+    def __init__(self, csv_file: str, progress_bar: bool = True, debug: bool = False):
+        self._csv_file = csv_file
+        self._progress_bar = progress_bar
+        self._debug = debug
+        self._df = self._get_dataframe()
+        self._last_timestamp = None
+        self.buys = []
+        self.fee = FEE
+        self.stats = Stats()
+
+    @abstractmethod
+    def init(self, *args, **kwargs) -> None:
+        """Setup any extra initialization here (e.g. apply ema to dataframe)"""
+        pass
+
+    @abstractmethod
+    def backtest(self, timestamp, row) -> None:
+        """Each row is fed into here for applying a backtest strategy on a stream of data"""
+        pass
+
+    def run(self) -> None:
+        """Run the backtest"""
+        for (timestamp, row) in self._next_row():
+            self.backtest(timestamp, row)
+
+
+    def _next_row(self) -> tuple:
+        """Generate from self._df.iterrows()"""
+        if self._progress_bar:
+            progress_bar = tqdm(total=len(self._df))
+            for (timestamp, row) in self._df.iterrows():
+                progress_bar.update(1)
+                self._last_timestamp = timestamp
+                yield timestamp, row
+        else:
+            for (timestamp, row) in self._df.iterrows():
+                yield timestamp, row
+
+    def _get_dataframe(self) -> pd.DataFrame:
+        """Read csv file using pandas and convert and set index to timestamp col
+
+        Expects form:
+            "timestamp","low","high","open","close","volume"
+        """
+        df = pd.read_csv(self._csv_file)
+        df.timestamp = pd.to_datetime(df.timestamp, unit='s')
+        df = df.set_index("timestamp")
+        df.dropna(axis='rows', how='any', inplace=True)
+        return df
+
+    def do_buy(self, price: Decimal):
+        self.buys.append(price)
+        size = self.stats.wallet / price
+
+    def do_sell(self, price: Decimal, buy_index: int):
+        fee = self.stats.wallet * self.fee
+        bought = self.buys[buy_index]
+        percent = pdiff(bought, price)
+        profit = self.stats.wallet * ((percent/100)-FEE)
+        self.stats.wallet = self.stats.wallet + profit
+        timestamp = self._last_timestamp
+        self.stats.sell_log.append(
+            (timestamp, huf(bought), huf(price), huf(profit), huf(percent), huf(self.stats.wallet))
+        )
+        del(self.buys[buy_index])
+        if profit > 0:
+            self.stats.wins += 1
+        else:
+            self.stats.losses += 1
+        year_month_day = str(timestamp).rsplit('-', 1)[0]
+        if not year_month_day in self.stats.per_day['fee']:
+            self.stats.per_day['fee'][year_month_day] = []
+        if not year_month_day in self.stats.per_day['percent']:
+            self.stats.per_day['percent'][year_month_day] = []
+        if not year_month_day in self.stats.per_day['net_profit']:
+            self.stats.per_day['net_profit'][year_month_day] = []
+        self.stats.per_day['fee'][year_month_day].append(fee)
+        self.stats.per_day['percent'][year_month_day].append(percent)
+        self.stats.per_day['net_profit'][year_month_day].append(profit)
+
+def backtest(
+        emaA: int = 1,
+        emaB: int = 2,
+        resample: str = '1D',
+        csv_file: str = None,
+        debug: bool = False,
+        c2c: bool = False,
+        dump_ohlc: bool = False,
+        strategy: str = 'emabot.strats.backtestema.BacktestEma') -> Stats:
+
+    backtest_cls = import_class(strategy)
+    backtester = backtest_cls(csv_file, debug=debug)
+    backtester.init(emaA=emaA, emaB=emaB, resample=resample)
+    if dump_ohlc:
+        print('Dumping OHLC:')
+        for (timestamp, row) in backtester._df.iterrows():
+            print(timestamp, row['close'].item(), row['emaA'].item(), row['emaB'].item())
+    else:
+        backtester.run()
+    return backtester.stats
 
 def main() -> None:
     parser = argparse.ArgumentParser()
@@ -28,6 +142,9 @@ def main() -> None:
     parser.add_argument('--ema-b',
         help='Set the emaB paramater (default:2)', dest='ema_b', required=False, default=2,
         type=int)
+    parser.add_argument('--strategy',
+        help='Select which strategy class to use from strategy/', dest='strategy',
+        required=False, default='emabot.strats.backtestema.BacktestEma', type=str)
     parser.add_argument('--debug',
         help='Enable debug output', dest='debug', action='store_true', required=False)
     parser.add_argument('--dump-ohlc',
@@ -41,7 +158,8 @@ def main() -> None:
         emaA=args.ema_a, emaB=args.ema_b, resample=args.resample,
         csv_file=args.csv_file,
         c2c=args.c2c, debug=args.debug,
-        dump_ohlc=args.dump_ohlc)
+        dump_ohlc=args.dump_ohlc,
+        strategy=args.strategy)
     if args.dump_ohlc:
         return
     fee_total = sum([sum(i) for i in stats.per_day['fee'].values()])
--- emabot/util.py
+++ emabot/util.py
@@ -1,104 +1,14 @@
-import warnings
 from decimal import Decimal
-import pandas as pd
-import pandas_ta as ta
-from tqdm import tqdm
-
-warnings.simplefilter(action='ignore', category=pd.errors.PerformanceWarning)
-
-FEE = Decimal(0.6/100)
-
-class Stats:
-    wallet: Decimal = Decimal('1000.00')
-    losses: int = 0
-    wins: int = 0
-    per_day: dict = {
-        'fee':{}, 'net_profit':{}, 'percent':{}
-    }
-    sell_log: list = []
-
-def pdiff(old, new):
-    return ((Decimal(new) - Decimal(old)) / Decimal(old)) * Decimal('100.0')
-
-def get_dataframes(csv_file: str, emaA: int = 1, emaB: int = 2, resample: str = '1D'):
-    df = pd.read_csv(csv_file)
-    df.timestamp = pd.to_datetime(df.timestamp, unit='s')
-    df = df.set_index("timestamp")
-    df = df.drop(columns=['open','high','low','volume'])
-    idf = df.resample(resample).ohlc()
-    df['emaA'] = ta.ema(idf['close']['close'], length=emaA)
-    df['emaB'] = ta.ema(idf['close']['close'], length=emaB)
-    df.fillna(method='ffill', inplace=True)
-    df.dropna(axis='rows', how='any', inplace=True)
-    return df
 
 def huf(f: Decimal):
     return '{:,.2f}'.format(f)
 
-def _backtest(df,
-        emaA: int = 1, emaB: int = 2, resample: str = '1D',
-        progress_bar: tqdm = None,
-        debug: bool = False, c2c: bool = False):
-    stats = Stats()
-    if c2c:
-        # If its a coin-to-coin (e.g. ETH-BTC), set to ~$1000 BTC @41k
-        stats.wallet = Decimal('0.0245')
-    bought = None
-    fee = None
-    for (timestamp, row) in df.iterrows():
-        progress_bar.update(1)
-        close = row['close'].item()
-        emaA = row['emaA'].item()
-        emaB = row['emaB'].item()
-        cur_price = Decimal(close)
-        if not bought and emaA > emaB:
-            bought = cur_price
-            size = stats.wallet / bought
-            # skip fee here and apply at end
-            fee = stats.wallet * FEE
-            if debug:
-                print('{} BOUGHT: price={:,.2f} A={:,.2f} B={:,.2f} '
-                    'size={:,.2f} fee={:,.2f} wallet={:,.2f}'.format(
-                        timestamp, cur_price, emaA,
-                        emaB, size, fee, stats.wallet))
-        elif bought and emaB > emaA:
-            percent = pdiff(bought, cur_price)
-            net_profit = stats.wallet * ((percent/100)-FEE)
-            stats.wallet = stats.wallet + net_profit
-            if debug:
-                print('{} SOLD : price={:,.2f} A={:,.2f} B={:,.2f} '
-                    'profit={:,.2f} percent={:,.2f} wallet={:,.2f}'.format(
-                        timestamp, cur_price, emaA,
-                        emaB, net_profit, percent, stats.wallet))
-            stats.sell_log.append(
-                (timestamp, huf(bought),
-                    huf(cur_price), huf(net_profit), huf(percent), huf(stats.wallet)))
-            if net_profit > 0:
-                stats.wins += 1
-            else:
-                stats.losses += 1
-            bought = None
-            year_month_day = str(timestamp).rsplit('-', 1)[0]
-            if not year_month_day in stats.per_day['fee']:
-                stats.per_day['fee'][year_month_day] = []
-            if not year_month_day in stats.per_day['percent']:
-                stats.per_day['percent'][year_month_day] = []
-            if not year_month_day in stats.per_day['net_profit']:
-                stats.per_day['net_profit'][year_month_day] = []
-            stats.per_day['fee'][year_month_day].append(fee)
-            stats.per_day['percent'][year_month_day].append(percent)
-            stats.per_day['net_profit'][year_month_day].append(net_profit)
-    return stats
+def pdiff(old, new):
+    return ((Decimal(new) - Decimal(old)) / Decimal(old)) * Decimal('100.0')
 
-def backtest(
-        emaA: int = 1, emaB: int = 2, resample: str = '1D',
-        csv_file: str = None, debug: bool = False, c2c: bool = False, dump_ohlc: bool = False):
-    df = get_dataframes(csv_file, emaA=emaA, emaB=emaB, resample=resample)
-    if dump_ohlc:
-        print('Dumping OHLC:')
-        for (timestamp, row) in df.iterrows():
-            print(timestamp, row['close'].item(), row['emaA'].item(), row['emaB'].item())
-    else:
-        with tqdm(total=len(df)) as progress_bar:
-            stats = _backtest(df, emaA=emaA, emaB=emaB, progress_bar=progress_bar, c2c=c2c, debug=debug)
-    return stats
+def import_class(cl: str):
+    """Import a class by name"""
+    d = cl.rfind(".")
+    classname = cl[d+1:len(cl)]
+    m = __import__(cl[0:d], globals(), locals(), [classname])
+    return getattr(m, classname)
